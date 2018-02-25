84d38f5c45beda53967b5157095029aa51b5c5ff
Jill Emard <emardj@oregonstate.edu>
2018-02-25 10:42:15 -0800

    random testing coverage

---
+++ projects/emardj/assignment-4/Calendar/src/test/java/calendar/CalDayRandomTest.java
@@ -0,0 +1,89 @@
+//Jillian Emard
+//ID:932510205
+package calendar;
+import java.util.Calendar;
+import java.util.Random;
+import java.util.GregorianCalendar;
+
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for CalDay class.
+ */
+
+public class CalDayRandomTest {
+	private static final long TestTimeout = 60 * 500 * 1; /* Timeout at 30 seconds */
+	private static final int NUM_TESTS=100;
+
+    /**
+     * Generate Random Tests that tests CalDay Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+			long startTime = Calendar.getInstance().getTimeInMillis();
+ 		 	long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
+
+			System.out.println("Start testing...");
+
+		 try{
+
+			 for (int iteration = 0; elapsed < TestTimeout; iteration++) {
+
+				 GregorianCalendar today = new GregorianCalendar(2018,12,25);
+				 CalDay calday = new CalDay(today);
+
+				 for (int i = 0; i < NUM_TESTS; i++) {
+				 long randomseed =System.currentTimeMillis(); //10
+	 //			System.out.println(" Seed:"+randomseed );
+				 Random random = new Random(randomseed);
+
+				 int startHour=ValuesGenerator.RandInt(random);
+				 int startMinute=ValuesGenerator.RandInt(random);
+				 int startDay=ValuesGenerator.RandInt(random);
+				 int startMonth=ValuesGenerator.getRandomIntBetween(random, 1, 11);
+				 int startYear=ValuesGenerator.RandInt(random);
+				 String title="Birthday Party";
+				 String description="This is my birthday party.";
+				 //Construct a new Appointment object with the initial data
+				 Appt appt = new Appt(startHour,
+									startMinute ,
+									startDay ,
+									startMonth ,
+									startYear ,
+									title,
+								 description);
+			 // if(!appt.getValid())continue;
+
+
+			 calday.addAppt(appt);
+		 }
+
+
+
+			 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+						if((iteration%10000)==0 && iteration!=0 )
+									System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
+
+			 }
+
+
+
+		 }catch(NullPointerException e){
+
+ 		}
+
+
+
+
+
+	 }
+
+
+
+}
